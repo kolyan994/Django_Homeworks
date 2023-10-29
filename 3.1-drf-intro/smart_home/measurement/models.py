d0974269cd3d7b36354831a5d03c2aa9ef42ceff
Николай <130489187+kolyan994@users.noreply.github.com>
2023-10-29 17:24:11 +0300

    Update models.py

--- 3.1-drf-intro/smart_home/measurement/models.py
+++ 3.1-drf-intro/smart_home/measurement/models.py
@@ -12,7 +12,7 @@ class Sensor(Model):
 
 
 class Measurement(Model):
-	sensor = models.ForeignKey(Sensor, on_delete=models.CASCADE)
+	sensor = models.ForeignKey(Sensor, on_delete=models.CASCADE, related_name='measurements')
 	temperature = models.DecimalField(max_digits=5, decimal_places=2)
 	measurement_date = models.DateTimeField(auto_now=False, auto_now_add=True)
 
